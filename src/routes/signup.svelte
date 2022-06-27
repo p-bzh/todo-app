675aae6dc32b5febf362cfe41e9b7a29c0f40989
p-bzh <ronan.questier@mailfence.com>
2022-06-27 09:48:21 +0300

    add login and signup

---
+++ src/routes/signup.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	<h2>Sign Up</h2>
+	<p>Already have an account ? <a href="/login">Sign In</a></p>
+	<form class="w-full">
+		<div class="flex flex-col my-2">
+			<label for="">Email</label>
+			<input class="leading-5" type="email" name="" id="" />
+		</div>
+		<div flex class="flex flex-col my-2">
+			<label for="">Password</label>
+			<input class="leading-5" type="password" name="" id="" />
+		</div>
+		<button class="w-full p-2 my-2 text-sm bg-purple-700 text-white rounded-lg">Sign Up</button>
+	</form>
+</div>
