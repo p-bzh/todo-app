8bd921a2343e1b411618ea1bd54646b69d8a8121
p-bzh <ronan.questier@mailfence.com>
2022-06-30 01:00:18 +0300

    change index page for auth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,11 @@
 	<h1 class="text-2xl font-bold text-center md:text-3xl">Todo App</h1>
 	<div class="flex flex-row justify-between">
 		<a
-			class="p-2 m-2 w-1/2 text-sm text-center no-underline outline outline-1 outline-purple-700 rounded-lg"
+			class="p-2 m-2 w-1/2 text-sm text-center no-underline outline outline-1 outline-purple-700"
 			href="/login">Sign In</a
 		>
 		<a
-			class="p-2 m-2 w-1/2 text-sm text-center no-underline bg-purple-700 text-white rounded-lg"
+			class="p-2 m-2 w-1/2 text-sm text-center no-underline bg-purple-700 text-white "
 			href="/signup">Sign Up</a
 		>
 	</div>
