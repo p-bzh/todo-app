835cbbc4dad86f4a50eb39021b49761e1ec1d21c
p-bzh <ronan.questier@mailfence.com>
2022-06-30 00:59:34 +0300

    update addTodo and loadTodo

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,22 +1,39 @@
 import { writable } from 'svelte/store';
-import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import {
+	getDocs,
+	collection,
+	addDoc,
+	deleteDoc,
+	doc,
+	updateDoc,
+	where,
+	query
+} from 'firebase/firestore';
 import { database } from '../firebase';
 
 export const tasks = writable([]);
 
-export const loadTasks = async () => {
-	const result = await getDocs(collection(database, 'tasks'));
-	tasks.set(result);
+export const loadTasks = async (user_id) => {
+	const q = query(collection(database, 'tasks'), where('user_id', '==', user_id));
+	const result = await getDocs(q);
+	result.forEach((task) => {
+		tasks.update((cur) => {
+			return [...cur, task.data()];
+		});
+	});
+	console.log(typeof tasks[0]);
 };
 
-export const addTodo = async (text) => {
+export const addTodo = async (text, user_id) => {
 	const task = {
+		user_id,
 		text,
 		completed: false,
-		created_at: new Date().toISOString()
+		created_at: new Date()
 	};
 	const response = await addDoc(collection(database, 'tasks'), task);
 	task.id = response.id;
+	console.log(task);
 	tasks.update((cur) => {
 		return [...cur, task];
 	});
